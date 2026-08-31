75e620b4be2def7bc79cfd363a040bb7fb9acb6d
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-08-31 17:44:41 -0400

    feat(organizar): Added organizar script

---
+++ tarea1/solution/organizar.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Argumentos inválidos. Uso: $0 <directorio>"
+	echo "Cantidad de argumentos inválida" >&2
+	exit 1
+fi
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	echo "Uso: $0 <directorio>"
+	exit 0
+fi
+
+if [ ! -d "$1" ]; then
+	echo "No existe el directorio $1" >&2
+	exit 2
+fi
+
+archivos=0
+categorias=""
+
+for archivo in "$1"/*; do
+	if [ -f "$archivo" ] &&
+	   [ "${archivo%%.*}" != "" ]; then
+		nombre="${archivo%%.*}"
+		extension="${archivo##*.}"
+		categoria="${extension,,}"
+		archivos=$((archivos + 1))
+		
+		if [ "${archivo##*.}" != "$archivo" ]; then
+			if ! grep -qx "$categoria" <<< "$categorias"; then
+				categorias="$categorias$categoria"$'\n'
+			fi
+
+			mkdir -p "$1"/"$categoria"/
+			mv "$archivo" "$1"/"$categoria"/
+		else
+			mkdir -p "$1"/"sin_extension"/
+			mv "$archivo" "$1"/"sin_extension"/
+		fi
+	fi
+done
+
+categorias="$(sort <<< $categorias)"
+lista_categorias=""
+
+for categoria in $(echo "$categorias"); do
+	lista_categorias="$lista_categorias$categoria "
+done
+
+cat << EOF
+Archivos organizados: $archivos
+Categorías: $lista_categorias
+EOF
+
+exit 0
