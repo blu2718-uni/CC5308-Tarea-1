f8cdb4dae5f81e52185bb04fb41fec4975312124
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-08-31 13:18:59 -0400

    feat(analizar_log): completed analizar_log.sh

---
+++ tarea1/solution/analizar_log.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Uso: $0 <ruta a log>" >&2
+	exit 1
+fi
+
+if [ ! -f "$1" ]; then
+	echo "El archivo $1 no existe" >&2
+	exit 2
+fi
+
+ruta=$1
+total_peticiones=$(wc -l < $ruta)
+codigos_http=$(cut -d" " -f9 $1)
+total_errores=$(wc -l <<< $(grep -v 200 <<< $codigos_http))
+ips=$(cut -d" " -f1 $1)
+ip_frecuente=$(grep -oE "[^ ]+" <<< $(head -n 1 <<< $(sort -nr <<< $(uniq -c <<< $ips))))
+ip_unicas=$(wc -l <<< $(uniq <<< $ips))
+
+cat << EOF
+Total de peticiones: $total_peticiones
+Peticiones con error: $total_errores
+IP con más peticiones: $(tail -n 1 <<< $ip_frecuente) ($(head -n 1 <<< $ip_frecuente) peticiones)
+Direcciones IP únicas: $ip_unicas
+EOF
